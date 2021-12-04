407b7393777f2cedec7689d3ddde0544f5859118
Orion Electron <electron.orion@gmail.com>
2021-12-04 11:26:56 +0545

    solved socket synchronization

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import io from 'socket.io-client';
+import styles from './componentCSS/index.module.css';
+import ChatContainer from './components/App';
+import SocketContext from './components/socketContext';
+
+
+const socket = io('http://localhost:4000', {
+  autoConnect: false
+});
+
+socket.onAny((event, ...args) => {
+  console.log(event, args);
+});
+
+
+function MainContainer() {
+
+
+  let textInput = React.createRef();
+  const [username, setUsername] = useState('');
+  socket.auth = {username};
+  const preventDefault = (e) => {
+    e.preventDefault();
+    console.log("Username selected!!");
+    setUsername(textInput.current.value);
+    socket.auth = { username: username };
+    socket.connect();
+
+  }
+  console.log("Chat container executed!!");
+
+
+
+  if (socket.auth.username!== '')
+    return <ChatContainer username={username} />
+
+
+  return (
+    <form onSubmit={preventDefault}>
+      <input ref={textInput} name="username" id="username" type="text" placeholder="Username" />
+      <br />
+      <button> Set Username </button>
+    </form>
+  );
+
+
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <SocketContext.Provider value={socket}>
+    <React.StrictMode>
+      <MainContainer />
+    </React.StrictMode>
+  </SocketContext.Provider>
+  ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
